9d9466e37a27bd50f1fe7f3abda095f7e3f88073
Mykola Diachok <ndyachok@gmail.com>
2025-01-21 08:31:15 +0100

    add loggining

--- raspberry/Program.cs
+++ raspberry/Program.cs
@@ -1,21 +1,40 @@
-﻿namespace raspberry;
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
 
-static class Program
+namespace raspberry;
+
+public class Program
 {
     static void Main(string[] args)
     {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddSerilog();
+        });
+
+        var logger = loggerFactory.CreateLogger<Program>();
+
+        logger.LogInformation("Application starting...");
+        
         var robotLight = new RobotLight();
         robotLight.Run();
-
+        logger.LogInformation("Starting Breath mode...");
         
         robotLight.Breath(70, 70, 255);
-        Thread.Sleep(15000);  
+        Thread.Sleep(5000);  
         robotLight.Pause();
         robotLight.FrontLight("off");
         
-        Thread.Sleep(10000);
+        logger.LogInformation("Starting Police mode...");
+        Thread.Sleep(5000);
         robotLight.Police();
-        Thread.Sleep(20000);
+        Thread.Sleep(5000);
         robotLight.FrontLight("off");
+        logger.LogInformation("Application ending...");
     }
 }
